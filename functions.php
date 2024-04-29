8e7782e019f01b93c33a46db493ec74bb1c0c474
Farid <farid@garuda.io>
2024-04-30 01:58:38 +0700

    Setup wrapper for block template

---
+++ functions.php
@@ -0,0 +1,23 @@
+<?php
+	// Custom functions and theme setup
+	function my_custom_theme_setup() {
+	    // Add theme support
+	    add_theme_support('title-tag');
+	    add_theme_support('post-thumbnails');
+	
+
+	    // Register menus
+	    register_nav_menus(array(
+	        'primary-menu' => 'Primary Menu',
+	    ));
+	}
+	add_action('after_setup_theme', 'my_custom_theme_setup');
+	
+
+	// Enqueue stylesheets and scripts
+	function my_custom_theme_scripts() {
+	    wp_enqueue_style('my-custom-theme-style', get_stylesheet_uri());
+	    // wp_enqueue_script('my-custom-theme-script', get_template_directory_uri() . '/js/main.js', array('jquery'), '1.0', true);
+	}
+	add_action('wp_enqueue_scripts', 'my_custom_theme_scripts');
+?>
