56db16e111419e521a9654e54921adb73034bcad
Farid <farid@garuda.io>
2024-05-01 20:31:30 +0700

    Add frontabout pattern to the homepage

---
+++ patterns/frontabout.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Title: Front About
+ * Slug: block-template/frontabout
+ * Categories: featured
+ */
+?>
+<!-- wp:group {"style":{"color":{"background":"#8cd5ec"},"spacing":{"blockGap":"0px","padding":{"right":"0","left":"0","top":"3rem","bottom":"0rem"}},"dimensions":{"minHeight":"500px"}},"layout":{"type":"constrained","contentSize":"1240px"}} -->
+<div class="wp-block-group has-background" style="background-color:#8cd5ec;min-height:500px;padding-top:3rem;padding-right:0;padding-bottom:0rem;padding-left:0"><!-- wp:columns -->
+<div class="wp-block-columns"><!-- wp:column {"verticalAlignment":"center","width":"66.6666%","style":{"spacing":{"padding":{"top":"0","bottom":"0","left":"0px","right":"0px"},"blockGap":"0"}},"layout":{"type":"constrained","justifyContent":"center"}} -->
+<div class="wp-block-column is-vertically-aligned-center" style="padding-top:0;padding-right:0px;padding-bottom:0;padding-left:0px;flex-basis:66.6666%"><!-- wp:group {"layout":{"type":"constrained","contentSize":"720px"}} -->
+<div class="wp-block-group"><!-- wp:heading {"textAlign":"right","level":1,"style":{"typography":{"fontSize":"48px"},"spacing":{"padding":{"top":"0","bottom":"0","left":"0","right":"0"}},"color":{"text":"#304f65"},"elements":{"link":{"color":{"text":"#304f65"}}}}} -->
+<h1 class="wp-block-heading has-text-align-right has-text-color has-link-color" style="color:#304f65;padding-top:0;padding-right:0;padding-bottom:0;padding-left:0;font-size:48px">Nama Kandidat</h1>
+<!-- /wp:heading -->
+
+<!-- wp:heading {"textAlign":"right","style":{"typography":{"fontSize":"14px"},"spacing":{"padding":{"top":"0","bottom":"0","left":"0","right":"0"},"margin":{"top":"0","bottom":"0","left":"0","right":"0"}},"elements":{"link":{"color":{"text":"#757575"}}},"color":{"text":"#757575"}},"fontFamily":"primary"} -->
+<h2 class="wp-block-heading has-text-align-right has-text-color has-link-color has-primary-font-family" style="color:#757575;margin-top:0;margin-right:0;margin-bottom:0;margin-left:0;padding-top:0;padding-right:0;padding-bottom:0;padding-left:0;font-size:14px">Bakal Posisi Kandidat</h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"align":"right","style":{"typography":{"fontSize":"20px"}}} -->
+<p class="has-text-align-right" style="font-size:20px">This page try to provide visual responses from the reference composition that has been compiled, refers to creative direction. Designing from color composition, use of appropriate fonts, to a visual composition that will appear in final output.</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"width":"33.333333333%"} -->
+<div class="wp-block-column" style="flex-basis:33.333333333%"><!-- wp:image -->
+<figure class="wp-block-image"><img alt=""/></figure>
+<!-- /wp:image --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns --></div>
+<!-- /wp:group -->
