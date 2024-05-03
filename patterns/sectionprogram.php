4f1f14942d85b6236866161a10ad4a785fcdd867
Farid <farid@garuda.io>
2024-05-03 21:31:20 +0700

    Update with profil page template

---
+++ patterns/sectionprogram.php
@@ -0,0 +1,109 @@
+<?php
+/**
+ * Title: Section Program
+ * Slug: block-template/sectionprogram
+ * Categories: featured
+ */
+?>
+
+<!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|60","bottom":"var:preset|spacing|60"}}},"layout":{"type":"constrained","contentSize":"1300px"}} -->
+<div class="wp-block-group" style="padding-top:var(--wp--preset--spacing--60);padding-bottom:var(--wp--preset--spacing--60)"><!-- wp:group {"style":{"spacing":{"padding":{"top":"0","bottom":"0"}}},"layout":{"type":"constrained","contentSize":"900px"}} -->
+<div class="wp-block-group" style="padding-top:0;padding-bottom:0"><!-- wp:heading {"textAlign":"center","level":1,"style":{"typography":{"fontSize":"48px"},"spacing":{"padding":{"top":"0","bottom":"var:preset|spacing|40","left":"0","right":"0"}},"color":{"text":"#304f65"},"elements":{"link":{"color":{"text":"#304f65"}}}}} -->
+<h1 class="wp-block-heading has-text-align-center has-text-color has-link-color" style="color:#304f65;padding-top:0;padding-right:0;padding-bottom:var(--wp--preset--spacing--40);padding-left:0;font-size:48px">Putra Daerah Sekaligus Mewakili Generasi Muda</h1>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"align":"center","style":{"typography":{"fontSize":"24px"}}} -->
+<p class="has-text-align-center" style="font-size:24px">Konten teks yang harus diisi, lengkapi sekitar 3-5 baris Konten teks yang harus diisi, lengkapi sekitar 3-5 baris Konten teks yang harus diisi, lengkapi sekitar 3-5 Konten teks yang harus diisi, lengkapi sekitar 3-5 baris.</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
+
+<!-- wp:separator {"style":{"spacing":{"margin":{"top":"var:preset|spacing|50","bottom":"var:preset|spacing|50"}}}} -->
+<hr class="wp-block-separator has-alpha-channel-opacity" style="margin-top:var(--wp--preset--spacing--50);margin-bottom:var(--wp--preset--spacing--50)"/>
+<!-- /wp:separator -->
+
+<!-- wp:columns {"verticalAlignment":"center","style":{"spacing":{"padding":{"top":"var:preset|spacing|40","bottom":"var:preset|spacing|40"}}}} -->
+<div class="wp-block-columns are-vertically-aligned-center" style="padding-top:var(--wp--preset--spacing--40);padding-bottom:var(--wp--preset--spacing--40)"><!-- wp:column {"verticalAlignment":"center","width":"33.33%","style":{"spacing":{"padding":{"right":"var:preset|spacing|50","left":"0"}}}} -->
+<div class="wp-block-column is-vertically-aligned-center" style="padding-right:var(--wp--preset--spacing--50);padding-left:0;flex-basis:33.33%"><!-- wp:group {"style":{"color":{"background":"#f16f70"},"border":{"radius":"20px"}},"layout":{"type":"constrained","contentSize":""}} -->
+<div class="wp-block-group has-background" style="border-radius:20px;background-color:#f16f70"><!-- wp:heading {"style":{"elements":{"link":{"color":{"text":"var:preset|color|white"}}},"typography":{"fontSize":"24px"}},"textColor":"white"} -->
+<h2 class="wp-block-heading has-white-color has-text-color has-link-color" style="font-size:24px">Bioskop untuk Warga Cimahi</h2>
+<!-- /wp:heading --></div>
+<!-- /wp:group --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"verticalAlignment":"center","width":"66.66%"} -->
+<div class="wp-block-column is-vertically-aligned-center" style="flex-basis:66.66%"><!-- wp:paragraph {"style":{"typography":{"fontSize":"20px"}}} -->
+<p style="font-size:20px">Penjelasan bioskop untuk warga Cimahi. Sediakan sekitar 3-5 baris disini. Penjalsan bioskop untuk warga Cimahi. Sediakan sekitar 3-5 baris disini. Penjalsan bioskop untuk warga Cimahi.</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns -->
+
+<!-- wp:columns {"verticalAlignment":"center","style":{"spacing":{"padding":{"top":"var:preset|spacing|40","bottom":"var:preset|spacing|40"}}}} -->
+<div class="wp-block-columns are-vertically-aligned-center" style="padding-top:var(--wp--preset--spacing--40);padding-bottom:var(--wp--preset--spacing--40)"><!-- wp:column {"verticalAlignment":"center","width":"33.33%","style":{"spacing":{"padding":{"right":"var:preset|spacing|50","left":"0"}}}} -->
+<div class="wp-block-column is-vertically-aligned-center" style="padding-right:var(--wp--preset--spacing--50);padding-left:0;flex-basis:33.33%"><!-- wp:group {"style":{"color":{"background":"#f16f70"},"border":{"radius":"20px"}},"layout":{"type":"constrained","contentSize":""}} -->
+<div class="wp-block-group has-background" style="border-radius:20px;background-color:#f16f70"><!-- wp:heading {"style":{"elements":{"link":{"color":{"text":"var:preset|color|white"}}},"typography":{"fontSize":"24px"}},"textColor":"white"} -->
+<h2 class="wp-block-heading has-white-color has-text-color has-link-color" style="font-size:24px">Kantung Parkir agar Kendaraan Aman Sentosa</h2>
+<!-- /wp:heading --></div>
+<!-- /wp:group --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"verticalAlignment":"center","width":"66.66%"} -->
+<div class="wp-block-column is-vertically-aligned-center" style="flex-basis:66.66%"><!-- wp:paragraph {"style":{"typography":{"fontSize":"20px"}}} -->
+<p style="font-size:20px">Penjelasan bioskop untuk warga Cimahi. Sediakan sekitar 3-5 baris disini. Penjalsan bioskop untuk warga Cimahi. Sediakan sekitar 3-5 baris disini. Penjalsan bioskop untuk warga Cimahi.</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns -->
+
+<!-- wp:columns {"verticalAlignment":"center","style":{"spacing":{"padding":{"top":"var:preset|spacing|40","bottom":"var:preset|spacing|40"}}}} -->
+<div class="wp-block-columns are-vertically-aligned-center" style="padding-top:var(--wp--preset--spacing--40);padding-bottom:var(--wp--preset--spacing--40)"><!-- wp:column {"verticalAlignment":"center","width":"33.33%","style":{"spacing":{"padding":{"right":"var:preset|spacing|50","left":"0"}}}} -->
+<div class="wp-block-column is-vertically-aligned-center" style="padding-right:var(--wp--preset--spacing--50);padding-left:0;flex-basis:33.33%"><!-- wp:group {"style":{"color":{"background":"#f16f70"},"border":{"radius":"20px"}},"layout":{"type":"constrained","contentSize":""}} -->
+<div class="wp-block-group has-background" style="border-radius:20px;background-color:#f16f70"><!-- wp:heading {"style":{"elements":{"link":{"color":{"text":"var:preset|color|white"}}},"typography":{"fontSize":"24px"}},"textColor":"white"} -->
+<h2 class="wp-block-heading has-white-color has-text-color has-link-color" style="font-size:24px">Penyediaan Pengelolaan Layanan Sampah Terbaik</h2>
+<!-- /wp:heading --></div>
+<!-- /wp:group --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"verticalAlignment":"center","width":"66.66%"} -->
+<div class="wp-block-column is-vertically-aligned-center" style="flex-basis:66.66%"><!-- wp:paragraph {"style":{"typography":{"fontSize":"20px"}}} -->
+<p style="font-size:20px">Penjelasan bioskop untuk warga Cimahi. Sediakan sekitar 3-5 baris disini. Penjalsan bioskop untuk warga Cimahi. Sediakan sekitar 3-5 baris disini. Penjalsan bioskop untuk warga Cimahi.</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns -->
+
+<!-- wp:columns {"verticalAlignment":"center","style":{"spacing":{"padding":{"top":"var:preset|spacing|40","bottom":"var:preset|spacing|40"}}}} -->
+<div class="wp-block-columns are-vertically-aligned-center" style="padding-top:var(--wp--preset--spacing--40);padding-bottom:var(--wp--preset--spacing--40)"><!-- wp:column {"verticalAlignment":"center","width":"33.33%","style":{"spacing":{"padding":{"right":"var:preset|spacing|50","left":"0"}}}} -->
+<div class="wp-block-column is-vertically-aligned-center" style="padding-right:var(--wp--preset--spacing--50);padding-left:0;flex-basis:33.33%"><!-- wp:group {"style":{"color":{"background":"#f16f70"},"border":{"radius":"20px"}},"layout":{"type":"constrained","contentSize":""}} -->
+<div class="wp-block-group has-background" style="border-radius:20px;background-color:#f16f70"><!-- wp:heading {"style":{"elements":{"link":{"color":{"text":"var:preset|color|white"}}},"typography":{"fontSize":"24px"}},"textColor":"white"} -->
+<h2 class="wp-block-heading has-white-color has-text-color has-link-color" style="font-size:24px">Jalan Mulus Anti Bolong</h2>
+<!-- /wp:heading --></div>
+<!-- /wp:group --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"verticalAlignment":"center","width":"66.66%"} -->
+<div class="wp-block-column is-vertically-aligned-center" style="flex-basis:66.66%"><!-- wp:paragraph {"style":{"typography":{"fontSize":"20px"}}} -->
+<p style="font-size:20px">Penjelasan bioskop untuk warga Cimahi. Sediakan sekitar 3-5 baris disini. Penjalsan bioskop untuk warga Cimahi. Sediakan sekitar 3-5 baris disini. Penjalsan bioskop untuk warga Cimahi.</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns -->
+
+<!-- wp:columns {"verticalAlignment":"center","style":{"spacing":{"padding":{"top":"var:preset|spacing|40","bottom":"var:preset|spacing|40"}}}} -->
+<div class="wp-block-columns are-vertically-aligned-center" style="padding-top:var(--wp--preset--spacing--40);padding-bottom:var(--wp--preset--spacing--40)"><!-- wp:column {"verticalAlignment":"center","width":"33.33%","style":{"spacing":{"padding":{"right":"var:preset|spacing|50","left":"0"}}}} -->
+<div class="wp-block-column is-vertically-aligned-center" style="padding-right:var(--wp--preset--spacing--50);padding-left:0;flex-basis:33.33%"><!-- wp:group {"style":{"color":{"background":"#f16f70"},"border":{"radius":"20px"}},"layout":{"type":"constrained","contentSize":""}} -->
+<div class="wp-block-group has-background" style="border-radius:20px;background-color:#f16f70"><!-- wp:heading {"style":{"elements":{"link":{"color":{"text":"var:preset|color|white"}}},"typography":{"fontSize":"24px"}},"textColor":"white"} -->
+<h2 class="wp-block-heading has-white-color has-text-color has-link-color" style="font-size:24px">Ruang Terbuka Hijau</h2>
+<!-- /wp:heading --></div>
+<!-- /wp:group --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"verticalAlignment":"center","width":"66.66%"} -->
+<div class="wp-block-column is-vertically-aligned-center" style="flex-basis:66.66%"><!-- wp:paragraph {"style":{"typography":{"fontSize":"20px"}}} -->
+<p style="font-size:20px">Penjelasan bioskop untuk warga Cimahi. Sediakan sekitar 3-5 baris disini. Penjalsan bioskop untuk warga Cimahi. Sediakan sekitar 3-5 baris disini. Penjalsan bioskop untuk warga Cimahi.</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns -->
+
+<!-- wp:buttons {"layout":{"type":"flex","justifyContent":"center"},"fontFamily":"secondary"} -->
+<div class="wp-block-buttons has-secondary-font-family"><!-- wp:button {"backgroundColor":"base","style":{"color":{"text":"#304f65"},"elements":{"link":{"color":{"text":"#304f65"}}},"border":{"color":"#8ed6ece6","width":"1px"},"typography":{"fontSize":"20px"}},"className":"is-style-fill","fontFamily":"secondary"} -->
+<div class="wp-block-button has-custom-font-size is-style-fill has-secondary-font-family" style="font-size:20px"><a class="wp-block-button__link has-base-background-color has-text-color has-background has-link-color has-border-color wp-element-button" style="border-color:#8ed6ece6;border-width:1px;color:#304f65">Kenali Gagasan</a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons --></div>
+<!-- /wp:group -->
